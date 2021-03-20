933d13169eb423600e7a4ed389d2e8b0d5ed0cf6
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-03-20 23:16:37 +0100

    added Models: TourItem and LogItem

---
+++ TourPlanner.Models/LogItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Models
+{
+    public class LogItem
+    {
+        public DateTime DateTime { get; set; }
+        public string Report { get; set; }
+        public string Distance { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        public float Rating { get; set; }
+    }
+}
---
+++ TourPlanner.Models/TourItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TourPlanner.Models
+{
+    public class TourItem
+    {
+        public string Name { get; set; }
+        public string TourDescription { get; set; }
+        public string RouteInformation { get; set; }
+        public float TourDistance { get; set; }
+    }
+}
