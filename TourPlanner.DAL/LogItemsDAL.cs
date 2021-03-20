0db6d68a631ab2d9311854a61401447d49f182a8
Joel Kudiyirickal <if19b226@technikum-wien.at>
2021-03-21 00:48:44 +0100

    added DAL: TourItemsDAL and LogItemsDAL

---
+++ TourPlanner.DAL/LogItemsDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TourPlanner.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.DAL
+{
+    public class LogItemsDAL
+    {
+        private string dbConnection;
+
+        public LogItemsDAL(string connectionString)
+        {
+            // establish connection to DB
+            dbConnection = connectionString;
+        }
+
+        public IEnumerable<LogItem> GetLogs(string tourname)
+        {
+            return new List<LogItem>() {
+
+            };
+        }
+    }
+}
